a21cfb13f4a9bc42146283c3e26ccc534a73addf
du Réau Martin <martin.du-reau@student-cs.fr>
2025-03-14 11:21:06 +0000

    update link axios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // Ajuste si tu veux pointer sur une URL différente 
 // (par exemple un proxy, ou un nom de domaine).
 // En local, si FastAPI tourne sur http://localhost:8000:
-const BASE_URL = process.env.REACT_APP_API_URL;
-
+//const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = "https://sybergueapidev01-dhf9e2bgesfdb3hc.westeurope-01.azurewebsites.net"
 export async function generateDoc({
   compte_rendu_file,
   description_missions,
